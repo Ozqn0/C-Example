a9fe0fa9d1435ebb4d10a9b55f07b5aef9649516
Ozqn0 <ozanyusuff135@gmail.com>
2024-12-25 02:06:42 +0300

    number guessing game with given range

---
+++ nubersgame.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int lower, upper, secret, guess;
+
+    // Kullanıcıdan aralık bilgilerini alma
+    printf("Tahmin oyunu için istediğiniz aralığı belirleyin.\n");
+    printf("Aralığın Alt Sınırı: ");
+    scanf("%d", &lower);
+    printf("Aralığın Üst Sınırı: ");
+    scanf("%d", &upper);
+
+    // Rastgele sayı oluşturma
+    srand(time(0));
+    secret = rand() % (upper - lower + 1) + lower;
+
+    printf("Belirlediğiniz sayı aralığında bir sayı tahmin ediniz (%d - %d): ", lower, upper);
+
+    // Tahmin döngüsü
+    do {
+        scanf("%d", &guess);
+
+        // Aralık dışı kontrolü
+        if (guess < lower || guess > upper) {
+            printf("Lütfen %d ile %d arasında bir sayı giriniz: ", lower, upper);
+        } else if (guess > secret) {
+            printf("Daha küçük bir sayı girin: ");
+        } else if (guess < secret) {
+            printf("Daha büyük bir sayı girin: ");
+        } else {
+            printf("Tebrikler! Doğru tahmin.\n");
+        }
+    } while (guess != secret);
+
+    return 0;
+}
